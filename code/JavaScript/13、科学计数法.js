3f38efc0c711f80d4d7b8feab43c0423282fc757
高启明 <gaoqiming@100tal.com>
2021-07-14 16:59:28 +0800

    feat: 每日一题

--- code/JavaScript/13、科学计数法.js
+++ code/JavaScript/13、科学计数法.js
@@ -1,1 +1,19 @@
 /**     2 实现   12345678.002345   ->   12,345,678.002345*/
+
+function blankNum(num) {
+  // 拆分小数
+  const blankArr = num.toString().split('.');
+  const sumNumber = blankArr[0];
+  let sumStr = [];
+  let flag = 0;
+  for (let i = blankArr[0].length - 1; i >= 0; i--) {
+    sumStr.unshift(sumNumber[i]);
+    flag++;
+    if (flag % 3 === 0 && i !== 0) {
+      sumStr.unshift(',');
+    }
+  }
+  console.log(sumStr);
+  return sumStr.join('') + `.${blankArr[1]}`;
+}
+console.log(blankNum(123456789.002345));
